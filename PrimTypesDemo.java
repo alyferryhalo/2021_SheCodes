63502fefd52c7d357bef1601501ba1e0a41ec3b4
Alisa Gorislav <61160686+alyferryhalo@users.noreply.github.com>
2021-05-21 01:01:04 +0500

    Create PrimTypesDemo.java

---
+++ PrimTypesDemo.java
@@ -0,0 +1,17 @@
+public class PrimTypesDemo {
+	
+	public static void main(String[] args) {
+		
+		// численные
+		byte b = 20; 				//   8 bits, 1 bytes
+		short s = 10; 			//  16 bits, 2 bytes
+		int i = 1000000;		//  32 bits, 4 bytes
+		long LONG = 64; 		//  64 bits, 8 bytes
+		double d = 23.89;		//           8 bytes - дефолтный тип!
+		float f = 10.9f; 		//           4 bytes
+		
+		// логические
+		
+		// символьные
+	}
+}
